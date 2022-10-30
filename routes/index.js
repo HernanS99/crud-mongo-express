1778dc8fbf13607238d4379a78dbc9c8bef0dae1
HernanS99 <hernan.silva2299@gmail.com>
2022-10-30 20:41:54 -0300

    add post and get, module export

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const {createUser,readUser,editUser,deleteUser} = require('../controllers/userControllers')
 const router = express.Router()
+
+router.route('/user').post(createUser).get(readUser)
+
+
+
+module.exports = router
