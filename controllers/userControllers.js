32b53e4d8a57571984f36bd2171b820157b65b9c
HernanS99 <hernan.silva2299@gmail.com>
2022-10-30 20:33:09 -0300

    add crud to usercontroller

---
+++ controllers/userControllers.js
@@ -0,0 +1,15 @@
+const createUser = () => {
+    console.log('crear usuario')
+}
+
+const readUser = () => {
+    console.log('crear usuario')
+}
+
+const editUser = () => {
+    console.log('crear usuario')
+}
+
+const deleteUser = () => {
+    console.log('crear usuario')
+}
