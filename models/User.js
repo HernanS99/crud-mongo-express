ff7bd9d55a3f07260e15a3534422a6610dcf6ba5
HernanS99 <hernan.silva2299@gmail.com>
2022-11-03 14:38:34 -0300

    add user schemma code and create model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
 const mongoose = ('mongoose')
+
+const userSchema = new mongoose.Schema({
+    nombre: String,
+    apellido: String,
+    correo: String,
+    edad: Number,
+    newsletter : Boolean,
+    isAdmin: Boolean
+})
+
+const User = mongoose.model('user',userSchema)
