57434c1de1eb4bc3c8f8a9a74283ffc81f2668b0
HernanS99 <hernan.silva2299@gmail.com>
2022-11-08 16:13:55 -0300

    add user age valitation

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     nombre: {type:String,required : true},
     apellido:{type:String,required : true},
     correo: {type:String,required : true},
-    edad: {type:Number,required : true},
+    edad: {type:Number,required : true, min: 18, max: 150},
     newsletter : {type:Boolean,required : true},
     isAdmin: {type:Boolean,required : true}
 })
