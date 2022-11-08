e56728502da79f639bef7a9c7281e1d44ad45cde
HernanS99 <hernan.silva2299@gmail.com>
2022-11-08 16:16:29 -0300

    add user more valitations

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    nombre: {type:String,required : true},
-    apellido:{type:String,required : true},
-    correo: {type:String,required : true},
-    edad: {type:Number,required : true, min: 18, max: 150},
+    nombre: {type:String,required : true, trim: true, lowercase:true},
+    apellido:{type:String,required : true, trim: true, lowercase:true},
+    correo: {type:String,required : true, trim: true, lowercase:true},
+    edad: {type:Number,required : true, min: 18, max: 99},
     newsletter : {type:Boolean,required : true},
     isAdmin: {type:Boolean,required : true}
 })
