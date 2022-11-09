7a7f6c6516367a68cd1672bf6bf5abe27c26372b
HernanS99 <hernan.silva2299@gmail.com>
2022-11-09 17:14:03 -0300

    add cart model and schema

--- models/Cart.js
+++ models/Cart.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    userId: {type: mongoose.Types.ObjectId, ref: user},
+    productsId:[{
+        productId: {type: mongoose.Types.ObjectId, ref:'product'},
+        quantity: {type: number}
+    }]
+})
