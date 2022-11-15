b3388877fbeebe9e5fee0553b64325cbfebeb252
HernanS99 <hernan.silva2299@gmail.com>
2022-11-15 14:07:01 -0300

    fixing erros

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
     correo: {type:String,required : true, trim: true, lowercase:true},
     edad: {type:Number,required : true, min: 18, max: 99},
     newsletter : {type:Boolean,required : true},
-    isAdmin: {type:Boolean,required : true},
+    isAdmin: {type:Boolean,required : true,default:false},
     password: {type:String,required:true,minLength:8},
     salt: {type: String, required: true}
 })
