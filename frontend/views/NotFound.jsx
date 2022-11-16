bb9df42d4ec11fa07981e2aa3d756461fbd4ef39
HernanS99 <hernan.silva2299@gmail.com>
2022-11-16 15:07:18 -0300

    add nÂot found img

---
+++ frontend/views/NotFound.jsx
@@ -0,0 +1,24 @@
+const NotFound = () => {
+    return(
+        <div className='container'>
+            <div className="d-flex align-items-center justify-content-center vh-100">
+                <div className="text-center row">
+                    <div className=" col-md-6">
+                        {/* <img src={tabla} alt=""
+                            className="img-fluid"/> */}
+                    </div>
+                    <div className=" col-md-6 mt-5">
+                        <p className="fs-3"> <span className="text-danger">Error</span> Pagina no encontrada.</p>
+                        <p className="lead">
+                            La pagina que buscas no existe
+                        </p>
+                        <a href="/" className="btn btn-primary">Ir a inicio</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
