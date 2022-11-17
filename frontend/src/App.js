7b626c8325bd2e62b9f21080ef0a43ce0dc95b9f
HernanS99 <hernan.silva2299@gmail.com>
2022-11-17 17:21:42 -0300

    add index js front end sort and nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,28 @@ import { Routes , Route, Navigate } from 'react-router-dom'
 import Login from './components/Login';
 import NotFound from './views/NotFound';
 import Home from './views/Home';
+import Nav from './components/Nav';
 
 function App() {
   return(
-  <Routes>
-    <Route
-      path='/' 
-      element={<Home />}
-    />
-    <Route
-      path='/Login'
-      element={<Login />}
-    />
-    <Route 
-    path='*' 
-    element={<NotFound/>}
-    />
-  </Routes>
+    <div>
+      <Nav>
+        <Routes>
+            <Route
+              path='/' 
+              element={<Home />}
+            />
+            <Route
+              path='/Login'
+              element={<Login />}
+            />
+            <Route 
+              path='*' 
+              element={<NotFound/>}
+            />
+        </Routes>
+      </Nav>
+    </div>
   )
 }
 
