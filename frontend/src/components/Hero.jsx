01a0522a101d657e60db539e73c92be5d651be4a
HernanS99 <hernan.silva2299@gmail.com>
2022-11-17 18:02:06 -0300

    add photos and hero carrusel imgs

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import Carousel from 'react-bootstrap/Carousel';
+import teclado from '../assets/img/teclado.jpg'
+import pcgamer from '../assets/img/pcgamer.jpg'
+import notebooks from '../assets/img/notebooks.jpg'
 
 const Hero = () => {
     return(
@@ -6,26 +9,33 @@ const Hero = () => {
             <div className="row ">
                 <div className="col-xl-5 col-md-12 p-5">
                     <h1 className='text-center fs-1 fw-bold'>Bienvenido</h1>
-                    <h4 className='mt-5'>Te invitamos a disfrutar de nuestra tienda de componentes computacionales! Lo mejor del hardware actual al mejor precio</h4>
+                    <h4 className='mt-5'>Te invitamos a disfrutar de nuestra tienda de componentes computacionales! Lo mejor del hardware actual al mejor precio.</h4>
                 </div>
                 <div className="col-xl-7 col-md-12 p-5">
                     <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
                         <Carousel variant="dark">
-                            {/* <Carousel.Item>
+                            <Carousel.Item>
                                 <img
                                     className="w-100 img-fluid"
                                     height=""
-                                    src={Sushi}
+                                    src={teclado}
                                     alt="First slide"
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
                                     className="w-100 img-fluid"
-                                    src={Front}
+                                    src={pcgamer}
                                     alt="Second slide"
                                 />
-                            </Carousel.Item> */}
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="w-100 img-fluid"
+                                    src={notebooks}
+                                    alt="Second slide"
+                                />
+                            </Carousel.Item>
                         </Carousel>
                     </div>
                 </div>
