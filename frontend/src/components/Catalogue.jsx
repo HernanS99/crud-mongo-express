cb7363b530a3f05f19f55d68bdbf7323b6431859
HernanS99 <hernan.silva2299@gmail.com>
2022-11-17 17:33:38 -0300

    add catalogue component

--- frontend/src/components/Catalogue.jsx
+++ frontend/src/components/Catalogue.jsx
@@ -1,6 +1,15 @@
 const Catalogue = () => {
     return(
-
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <p className="fs-1 fw-bold text-center">Catalogo</p>
+                </div>
+            </div>
+            <div className="row ">
+             {/* { productos.map(producto=><Card datos={producto} key={producto.id}/>) }  */}
+            </div>
+        </div>
     )
 }
 
