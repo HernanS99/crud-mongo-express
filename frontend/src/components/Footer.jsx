ed34299956df194e1deb6eb74862387145ee7b4c
HernanS99 <hernan.silva2299@gmail.com>
2022-11-17 17:26:20 -0300

    add footer code

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,58 @@
 const Footer = () => {
     return(
+    <div className='container-fluid'>
+      <footer className="row text-center text-white">
+        <div className="container pt-4 bg-secondary">
+          <div className="mb-4">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-facebook-f"></i
+            ></a>
 
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-twitter"></i
+            ></a>
+
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-google"></i
+            ></a>
+
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-instagram"></i
+            ></a>
+
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-linkedin"></i
+            ></a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+            ><i className="fab fa-github"></i
+            ></a>
+          </div>
+
+        </div>
+        <div className="text-center text-dark p-3 bg-secondary bg-opacity-25">
+          © 2020 Copyright:
+          <a className="text-dark" href="#!">Hernan Silva</a>
+        </div>
+      </footer>
+    </div>
     )
 }
 export default Footer
