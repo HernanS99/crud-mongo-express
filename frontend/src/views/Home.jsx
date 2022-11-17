75b07e3a560246ac43d7ea0839c4b22cf4adda74
HernanS99 <hernan.silva2299@gmail.com>
2022-11-17 17:37:36 -0300

    add catalogue component

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero"
 import Aboutus from "../components/Aboutus"
 import Contact from "../components/Contact"
+import Catalogue from "../components/Catalogue"
 
 const Home = () => {
     return(
         <div>
             <Hero />
+            <Catalogue />
             <Aboutus />
             <Contact />
         </div>
