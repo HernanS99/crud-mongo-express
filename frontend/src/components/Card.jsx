7cc8e0cc3157de336bedc22a342f948b2eeea737
HernanS99 <hernan.silva2299@gmail.com>
2022-11-18 12:28:59 -0300

    add card code

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,22 @@
 const Card = () => {
     return(
-
+            <div className="col-sm-6 col-md-4 col-xl-3">
+                <div className="card-sl mb-5">
+                    <div className="card-image">
+                        <img
+                            src={img === null ? nf : img} alt='foto' className="img-fluid"/>
+                    </div>
+                    <div className="card-heading">
+                        {nombre}
+                    </div>
+                    <div className="card-text">
+                        {descripcion}
+                    </div>
+                    <div className="card-text fw-bold">
+                        ${precio}
+                    </div>
+                </div>
+            </div>
     )
 }
 
