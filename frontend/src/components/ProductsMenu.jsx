82ff6f722537d7d9bda079da77e37f64150b7289
HernanS99 <hernan.silva2299@gmail.com>
2022-11-18 13:41:25 -0300

    add products menu code

--- frontend/src/components/ProductsMenu.jsx
+++ frontend/src/components/ProductsMenu.jsx
@@ -1,6 +1,34 @@
 const ProductsMenu = () => {
     return (
-        
+        <div>
+            <div>
+                <p className="text-center fs-1 ">Menu de productos</p>
+            </div>
+            <div>
+                <AddProduct update={getProducts}/>
+            </div>
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Precio</th>
+                            <th scope="col">Descripcion</th>
+                            <th scope="col">Url imagen</th>
+                            <th scope="col">Editar</th>
+                            <th scope="col">Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {productos.map(product => <ProductsRow datos={product} update={getProducts} key={product.id} />)}
+                    </tbody>
+                </table>
+            </div>
+            <div className="text-center mb-3">
+                <button type="button" className="btn btn-success" onClick={() => signOutUser()}>Cerrar Sesion</button>
+            </div>
+        </div>
     )
 }
 export default ProductsMenu
