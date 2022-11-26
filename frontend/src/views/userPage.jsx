a74d66a1b5fd4ead623738e7d084f781e6432b66
HernanS99 <hernan.silva2299@gmail.com>
2022-11-26 13:50:00 -0300

    add profile info

--- frontend/src/views/userPage.jsx
+++ frontend/src/views/userPage.jsx
@@ -6,7 +6,7 @@ const UserPage = () => {
     const context = useContext(UserContext)
     const { token , user  } = context.userState
     const [userr, setUserr] = useState(user)
-
+    console.log(user)
     useEffect(()=>{
         context.getUserInfo(token)
     },[])
@@ -15,7 +15,7 @@ const UserPage = () => {
             <div class="container rounded bg-white mt-5 mb-5">
                 <div class="row">
                     <div class="col-md-5 border-right">
-                        <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"></img><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+                        <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"></img><span class="font-weight-bold">{user.nombre}</span><span class="text-black-50">{user.correo}</span><span> </span></div>
                     </div>
                     <div class="col-md-7 border-right">
                         <div class="p-3 py-5">
@@ -23,12 +23,12 @@ const UserPage = () => {
                                 <h4 class="text-right">Datos de usuario</h4>
                             </div>
                             <div class="row mt-2">
-                                <div class="col-md-6"><label class="labels"></label><input type="text" class="form-control" placeholder="first name"></input></div>
-                                <div class="col-md-6"><label class="labels">Apellido</label><input type="text" class="form-control" placeholder="surname"></input></div>
+                                <div class="col-md-6"><label class="labels">Nombre</label><input type="text" class="form-control" placeholder="Nombre" value={user.nombre}></input></div>
+                                <div class="col-md-6"><label class="labels">Apellido</label><input type="text" class="form-control" placeholder="Apellido" value={user.apellido}></input></div>
                             </div>
                             <div class="row mt-3">
-                                <div class="col-md-12"><label class="labels">Edad</label><input type="text" class="form-control" placeholder="enter phone number"></input></div>
-                                <div class="col-md-12"><label class="labels">Correo</label><input type="text" class="form-control" placeholder="enter email id"></input></div>                            </div>
+                                <div class="col-md-12"><label class="labels">Edad</label><input type="text" class="form-control" placeholder="Edad" value={user.edad}></input></div>
+                                <div class="col-md-12"><label class="labels">Correo</label><input type="text" class="form-control" placeholder="correo" value={user.correo}></input></div>                            </div>
                             <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
                         </div>
                     </div>
