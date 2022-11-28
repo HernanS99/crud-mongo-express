1cdf5a90c2cfbdb41f958796211b3ba0afcc240f
HernanS99 <hernan.silva2299@gmail.com>
2022-11-28 15:01:19 -0300

    add fix to product controller

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,9 @@
 const mongoose = require ('mongoose')
 
 const productSchema = new mongoose.Schema({
-    name: {type:String,required : true, trim: true, lowercase:true},
+    name: {type:String,required : true, trim: true},
     price: {type:Number,required : true},
-    brand: {type:String,required : true, trim: true, lowercase:true},
+    brand: {type:String,required : true, trim: true},
     stock: {type:Number,required : true},
     desc: {type:String,required : true},
     imgUrl:{type:String,required : false}
