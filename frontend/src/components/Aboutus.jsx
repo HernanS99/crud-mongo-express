5ed1f4cabde290c7dbad5ef250e8adc3f32e084d
HernanS99 <hernan.silva2299@gmail.com>
2022-11-29 15:20:45 -0300

    fixing text of aboutus

--- frontend/src/components/Aboutus.jsx
+++ frontend/src/components/Aboutus.jsx
@@ -14,13 +14,13 @@ const Aboutus = () => {
                         Tenemos todo dispuesto para entregar una experiencia única.
                         </p>
                         <ul>
-                            <p><i className="fa-solid fa-check-double"></i>Contamos con expertos en la cocina que te cautivaran con los sabores más tradicionales extradios directamente desde Japon, con el objetivo de brindar, con el objetivo de brindar una experencia satisfactoria todos tus sentidos.</p>
-                            <p><i className="fa-solid fa-check-double"/> Contamos con una amplia variedad de comida.</p>
+                            <p><i className="fa-solid fa-check-double"></i>Contamos con componentes de todo tipo para el armado de computadores, desde tarjetas graficas hasta fuentes de poder</p>
+                            <p><i className="fa-solid fa-check-double"/> Contamos con una amplia variedad de marcas de tope de gama.</p>
                         </ul>
                         <div className="row icon-boxes">
                             <div className="col-md-6">
                                 <h4>Calidad</h4>
-                                <p>Contamos con una amplia carta para deleitarse con todos los gustos.</p>
+                                <p>Contamos con un equipo profesional que se encarga del perfecto funcionamiento de cada componente.</p>
                             </div>
                             <div className="col-md-6 mt-4 mt-md-0">
                                 <h4>Tiempo de entrega</h4>
