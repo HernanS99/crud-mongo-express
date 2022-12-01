86af476ac8ae1e89724028cb9057dd31c211d216
HernanS99 <hernan.silva2299@gmail.com>
2022-12-01 13:35:46 -0300

    add cartprovider

--- frontend/src/context/CartProvider.js
+++ frontend/src/context/CartProvider.js
@@ -3,14 +3,23 @@ import { useReducer } from "react"
 import cartReducers from "./CartReducer"
 
 
-const CartProvider = ({children}) => {
-    const [cartState, dispatch] = useReducer(cartReducers,{cart: []})
-       const addToCart = (id) =>{
-        dispatch({type: 'ADD', payload : id})
-       }
-    return (
-        <CartContext.Provider value={{cartState, addToCart}}> {children} </CartContext.Provider>
-    )
+
+import CartContext from './CartContext'
+import { useReducer } from 'react'
+import cartReducers from './CartReducers'
+
+const CartProvider = ({ children }) => {
+  const [cartState, dispatch] = useReducer(cartReducers, { cart: [], qty: 0 })
+
+  const addToCart = (item, qty) => {
+    dispatch({ type: 'ADD', payload: { item, qty } })
+  }
+
+  const deleteFromCart = (id) => {
+    dispatch({ type: 'DELETE', payload: id })
+  }
+
+  return <CartContext.Provider value={{ cartState, addToCart, deleteFromCart }}>{children}</CartContext.Provider>
 }
 
 export default CartProvider
