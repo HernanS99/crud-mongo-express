8220a044cf3a4b47b00405ffb272b342674961ff
HernanS99 <hernan.silva2299@gmail.com>
2022-12-01 12:28:27 -0300

    add cart button

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,18 +2,21 @@ import nf from '../assets/img/nf.png'
 import { NavLink, Link } from 'react-router-dom'
 
 const Card = (props) => {
-    const { brand, inStock, name, price, _id, imgUrl } = props.datos
-
+    const { brand, inStock, name, price, _id, imgUrl,stock } = props.datos
     const formatter = new Intl.NumberFormat('es-CL', {
         style: 'currency',
         currency: 'CLP',
       });
+      
+    const addToCart = () =>{
+        console.log(_id)
+    }
 
     return (
         
-            <div className="col-sm-6 col-md-4 col-xl-3">
+            <div className="col-sm-6 col-md-4 col-xl-3 ">
                 <Link to={`/products/${(_id)}`} className="style">
-                <div className="card-sl mb-5">
+                <div className="card-sl mb-5 ">
                     <div className="card-image">
                         <img
                             src={!imgUrl ? nf :  imgUrl} alt='foto' className="img-fluid" />
@@ -27,8 +30,12 @@ const Card = (props) => {
                     <div className="card-text fw-bold">
                         {formatter.format(price)}
                     </div>
+                    
                 </div>
                 </Link>
+                <div className="card-text fw-bold mb-3 d-grid gap-2">
+                        <button className="btn btn-primary" onClick={addToCart} disabled={stock > 0 ? false : true}>Agregar al carro</button>
+                </div>
             </div>
     )
 }
